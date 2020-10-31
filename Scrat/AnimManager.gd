2f54b96800b479fad8f52fd291df21fb3347b351
Fupi <fupicat@gmail.com>
2020-10-31 15:59:04 -0300

    Adicionei o q eu já fiz

---
+++ Scrat/AnimManager.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+# warning-ignore:unused_signal
+signal action_done
+
+const EYESLIP = 0.2
+
+onready var animtree = $AnimTree
+onready var walk_idle_blend = "parameters/walk_idle/blend_position"
+onready var idle_sm = "parameters/walk_idle/0/playback"
+onready var walk_sm = "parameters/walk_idle/1/playback"
+onready var one_shot = "parameters/one_shot/active"
+onready var action_sm = "parameters/actions/playback"
+onready var eyes_pos = "parameters/eyes_pos/blend_position"
+
+func _ready():
+    animtree.active = true
+    eyes_look_at()
+
+func walk_idle_state(variation := "Normal"):
+    animtree[idle_sm].travel(variation)
+    animtree[walk_sm].travel(variation)
+
+func play_one_shot(anim : String):
+    animtree[one_shot] = true
+    animtree[action_sm].start(anim)
+
+func current_action():
+    return animtree[action_sm].get_current_node()
+
+func blend_walk_idle(blend : float):
+    animtree[walk_idle_blend] = blend
+
+func eyes_look_at(pos := $Head.to_global(Vector2(500, 0))):
+    pos = $Head.to_local(pos)
+    pos.x = range_lerp(pos.x, -500, 500, -1, 1)
+    pos.y = range_lerp(pos.y, 500, -500, -1, 1)
+    var prev = animtree[eyes_pos]
+    animtree[eyes_pos] = lerp(prev, pos, EYESLIP)
